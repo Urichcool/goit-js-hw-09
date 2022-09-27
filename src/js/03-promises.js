2cf2f31525f92db2e275c6397ba1310c5cd4a9f3
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-09-27 16:27:18 +0200

    task 3 - done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+const firstDelay = document.querySelector('[name = delay]');
+const delayStep = document.querySelector('[name = step]');
+const amount = document.querySelector('[name = amount]');
+
+
+formEl.addEventListener('submit', event => {
+  let positionCount = 0;
+  let delayCount = Number(firstDelay.value) - Number(delayStep.value);
+  event.preventDefault();
+    setTimeout(() => {
+    timerId = setInterval(() => {
+      delayCount += Number(delayStep.value);
+      positionCount += 1;
+      createPromise(positionCount, delayCount)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
+      if (positionCount >= amount.value) {
+        clearInterval(timerId);
+        positionCount = 0;
+        delayCount = 0;
+      }
+    }, Number(delayStep.value));
+  }, Number(firstDelay.value) - Number(delayStep.value));
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resole, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+     if (shouldResolve) {
+        resole({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    });
+};
